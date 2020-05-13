d97372d025bc195756ada3d339b7366b69182f20
sale992 <aleksandarpetrovic092@gmail.com>
2020-05-13 10:37:52 +0200

    first

---
+++ swiper.js
@@ -0,0 +1,17 @@
+// var swiper = new Swiper('.swiper-container', {
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//       renderBullet: function (index, className) {
+//         return '<span class="' + className + '">' + (index + 1) + '</span>';
+//       },
+//     },
+//   });
+
+
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
